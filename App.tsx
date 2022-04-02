3c8046c1203be31ca33743417cabfb20d6248557
Bruno Becoski <brunobecoski@gmail.com>
2022-04-01 21:28:34 -0300

    Configurando o Sentry

--- App.tsx
+++ App.tsx
@@ -3,6 +3,15 @@ import { StatusBar } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 import codePush from 'react-native-code-push';
 
+import * as Sentry from "@sentry/react-native";
+
+Sentry.init({
+  dsn: "https://6b8105312c7a4882a9021bfb09accc18@o1185744.ingest.sentry.io/6305709",
+  // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+  // We recommend adjusting this value in production.
+  tracesSampleRate: 1.0,
+});
+
 import { Home } from './src/pages/Home';
 
 function App() {
@@ -11,6 +20,8 @@ function App() {
     codePush.sync({
       installMode: codePush.InstallMode.IMMEDIATE
     });
+
+    // throw new Error('Não foi possível abrir a aplicação. Não fique triste, tente mais tarde.');
 
     SplashScreen.hide();
   }, []);
